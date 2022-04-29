81ff8514fa430c5bff5c8fc17f886395ff372a3c
Flávio Sousa <root@fjsousa.com>
2022-04-29 16:03:50 +0100

    clean 10

--- clean-details.clj
+++ clean-details.clj
@@ -1,11 +1,32 @@
 (ns clean-details
   (:require [clojure.edn :as edn]
-            [clojure.pprint :as pprint]))
+            [clojure.pprint :as pprint]
+            [clojure.string :as str]))
 
 (def d (-> "enrich-raw.edn" slurp edn/read-string))
 
-(def results (take 10 d))
+(defn clean-name [s]
+  (update s :title-raw str/trim))
 
-(def results d)
+(defn clean-nec [s]
+  (update s :nec-raw #(edn/read-string (re-find #"\d+" %))))
 
-(spit "./enrich-clean.edn (with-out-str (pprint/pprint results))")
+(defn clean-address [s]
+  (update s :address-raw str/trim))
+
+(def results (->> d
+                  (take 10)
+                  (map clean-name)
+                  (map clean-nec)
+                  (map clean-address)
+                  (map clean-name)))
+
+
+#_(pprint/pprint results)
+
+#_(map :title-raw results)
+
+
+(spit "./enrich-clean.edn" (with-out-str (pprint/pprint results)))
+
+(spit "./table.edn" (with-out-str (pprint/print-table results)))
