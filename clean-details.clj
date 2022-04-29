0aaab9d37d16b26e1abe2ef8bf72f03b3ae6d412
Flávio Sousa <root@fjsousa.com>
2022-04-29 16:33:45 +0100

    clean weird char

--- clean-details.clj
+++ clean-details.clj
@@ -5,21 +5,26 @@
 
 (def d (-> "enrich-raw.edn" slurp edn/read-string))
 
+(defn clean-weirdness [s]
+  (apply str (remove #(= % \ ) (seq s))))
+
 (defn clean-name [s]
-  (update s :title-raw str/trim))
+  (update s :title-raw #(clean-weirdness (str/trim %))))
 
 (defn clean-nec [s]
   (update s :nec-raw #(edn/read-string (re-find #"\d+" %))))
 
+(comment
+  (clean-weirdness "Av Dr. António Caldas, loja 139, Faquelo  4970-592 Arcos de"))
+
 (defn clean-address [s]
-  (update s :address-raw str/trim))
+  (update s :address-raw #(clean-weirdness (str/trim %))))
 
 (def results (->> d
                   (take 10)
                   (map clean-name)
                   (map clean-nec)
-                  (map clean-address)
-                  (map clean-name)))
+                  (map clean-address)))
 
 
 #_(pprint/pprint results)
@@ -29,4 +34,4 @@
 
 (spit "./enrich-clean.edn" (with-out-str (pprint/pprint results)))
 
-(spit "./table.edn" (with-out-str (pprint/print-table results)))
+(spit "./table.txt" (with-out-str (pprint/print-table results)))
