21dd5bac5dcc6464eb6bc3f0e5a327d78489fd6e
Flávio Sousa <root@fjsousa.com>
2022-05-01 10:02:43 +0100

    enrich raw proper nec parse

--- clean-details.clj
+++ clean-details.clj
@@ -9,7 +9,11 @@
   (apply str (remove #(= % \ ) (seq s))))
 
 (defn clean-name [s]
-  (update s :title-raw #(clean-weirdness (str/trim %))))
+  (-> s
+      (assoc :title-clean (:title-raw s))
+      (update :title-clean #(clean-weirdness (str/trim %)))
+      (dissoc :title-raw)
+      ))
 
 (defn clean-nec [s]
   (update s :nec-raw #(edn/read-string (re-find #"\d+" %))))
@@ -18,20 +22,65 @@
   (clean-weirdness "Av Dr. António Caldas, loja 139, Faquelo  4970-592 Arcos de"))
 
 (defn clean-address [s]
-  (update s :address-raw #(clean-weirdness (str/trim %))))
+  (->
+   s
+   (assoc :address-clean (:address-raw s))
+   (update :address-clean #(clean-weirdness (str/trim %)))
+   (dissoc :address-raw)))
 
 (def results (->> d
-                  (take 10)
+                  (take 47)
                   (map clean-name)
                   (map clean-nec)
                   (map clean-address)))
-
+(last (take 48 d))
 
 #_(pprint/pprint results)
 
 #_(map :title-raw results)
 
 
-(spit "./enrich-clean.edn" (with-out-str (pprint/pprint results)))
+#_(spit "./enrich-clean.edn" (with-out-str (pprint/pprint results)))
 
-(spit "./table.txt" (with-out-str (pprint/print-table results)))
+#_(spit "./table.txt" (with-out-str (pprint/print-table results)))
+
+
+(-> {:distrito "Bragança",
+ :concelho "Carrazeda de Ansiâes",
+ :school-href
+ "https://www.imt-ip.pt/sites/IMTT/Portugues/EnsinoConducao/LocalizacaoEscolasConducao/Paginas/K%c3%a9ris.aspx",
+ :concelho-href
+ "https://www.imt-ip.pt/sites/IMTT/Portugues/EnsinoConducao/LocalizacaoEscolasConducao/Paginas/LocalizacaoEscolasConducao.aspx?Distrito=Braganca&Concelho=Carrazeda%20Ans%C3%A3es",
+ :title-raw "\r\n\t        Kéris \r\n\t        ",
+ :address-raw
+ "\r\n\t\t\tRua Luis de Camões, n.º 319, r/c B 5140-080 Carrazeda de Ansiães \r\n\t\t",
+ :nec-raw
+ {:type :element,
+  :attrs
+  {:id "ctl00_PlaceHolderMain_ObservacoesDiv",
+   :class "paginaConteudoDivTextoSemImagem"},
+  :tag :div,
+  :content
+  ["\r\n\t\t\t"
+   {:type :element,
+    :attrs
+    {:id
+     "ctl00_PlaceHolderMain_Observacoes__ControlWrapper_RichHtmlField",
+     :style "display:inline"},
+    :tag :div,
+    :content
+    [{:type :element,
+      :attrs {:color "#808184", :face "Arial"},
+      :tag :font,
+      :content
+      ["Nº Alvará: "
+       {:type :element,
+        :attrs {:color "#808184", :face "Arial"},
+        :tag :font,
+        :content ["1356"]}
+       " "]}]}
+   "\r\n\t\t"]}}
+    clean-name
+    clean-address
+    clean-nec
+    )
--- pull_details.clj
+++ pull_details.clj
@@ -4,7 +4,7 @@
 
 (require '[babashka.pods :as pods])
 (pods/load-pod 'retrogradeorbit/bootleg "0.1.9")
-(require '[clojure.walk :refer [postwalk]])
+(require '[clojure.walk :refer [postwalk prewalk]])
 (require '[babashka.curl :as curl])
 
 (require
@@ -16,7 +16,7 @@
            slurp
            (json/parse-string true)))
 
-(defn parse-nec
+#_(defn parse-nec
   "nec is problematic and shows up in different ways.
   These rules are 'low effort' and subject to change"
   [el]
@@ -29,6 +29,49 @@
       parsed
       el)))
 
+(defn parse-nec-new
+  "pull :content value if string? or if the first element is a string"[el]
+  (let [a (atom [])]
+    (postwalk (fn [{:keys [content] :as el}]
+                (when (string? content)
+                  (swap! a conj content))
+                (when (string? (first content))
+                  (swap! a conj (first content)))
+                el)
+              el)
+    (apply str @a)))
+
+#_(def first-level (-> el :content rest first :content first))
+
+#_#_(def el
+      {:type :element,
+       :attrs
+       {:id "ctl00_PlaceHolderMain_ObservacoesDiv",
+        :class "paginaConteudoDivTextoSemImagem"},
+       :tag :div,
+       :content
+       ["\r\n\t\t\t"
+        {:type :element,
+         :attrs
+         {:id
+          "ctl00_PlaceHolderMain_Observacoes__ControlWrapper_RichHtmlField",
+          :style "display:inline"},
+         :tag :div,
+         :content ;;first-level
+         [{:type :element,
+           :attrs {:color "#808184", :face "Arial"},
+           :tag :font,
+           :content
+           ["Nº Alvará: "
+            {:type :element,
+             :attrs {:color "#808184", :face "Arial"},
+             :tag :font,
+             :content ["1356"]}
+            " "]}]}
+        "\r\n\t\t"]})
+
+(parse-nec-new [el])
+
 (defn hiccup->school-raw [hc]
   (let [r (atom {})]
    (postwalk (fn [{:keys [attrs] :as el}]
@@ -39,10 +82,9 @@
                  (swap! r assoc :address-raw (-> el :content first)))
                (when (and (= (:class attrs) "paginaConteudoDivTextoSemImagem")
                           (= 2 (count (keys attrs))))
-                 (swap! r assoc :nec-raw (parse-nec el)))
+                 (swap! r assoc :nec-raw (parse-nec-new el)))
                el) hc)
    @r))
-
 
 (defn href->hc! [href]
   (let [html (curl/get href {:raw-args ["-k"]})
@@ -60,3 +102,8 @@
   (Thread/sleep (int (rand 500))))
 
 (spit "./enrich-raw.edn" (with-out-str (pprint/pprint @results)))
+
+#_(->
+ "https://www.imt-ip.pt/sites/IMTT/Portugues/EnsinoConducao/LocalizacaoEscolasConducao/Paginas/K%c3%a9ris.aspx"
+ href->hc!
+  hiccup->school-raw)
