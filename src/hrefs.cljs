78727a0215607f830804a114abae3081ee61ae54
Flávio Sousa <root@fjsousa.com>
2024-01-20 16:37:00 +0000

    fix hrefs maybe

--- src/hrefs.cljs
+++ src/hrefs.cljs
@@ -1,9 +1,36 @@
+;; Not very smart but does the job
+
 (ns hrefs
+  {:clj-kondo/config '{:lint-as {promesa.core/let clojure.core/let
+                                 example/defp clojure.core/def}}}
   (:require
    ["puppeteer$default" :as puppeteer]
    [promesa.core :as p]
    ["fs" :as fs]
    [clojure.string :as str]))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Resources
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(def base-url "https://www.imt-ip.pt")
+
+(def district-url "/sites/IMTT/Portugues/EnsinoConducao/LocalizacaoEscolasConducao/Paginas/LocalizacaoEscolasConducao.aspx")
+
+;; It's a query to get the links in a page 'a'
+(def anchors-query "var a = []; Array.from(document.querySelectorAll('a')).forEach(el => {a.push({href: el.href, id: el.id, html: el.innerHTML })});a")
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; utils
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defmacro defp
+  "Define var when promise is resolved"
+  [binding expr]
+  `(-> ~expr (.then (fn [val]
+                      (def ~binding val)))))
+
+
 
 (defn rand-int-from-to
   "returns a random integer from to"
@@ -13,7 +40,11 @@
     (+ min-value (rand-int  (- max-value min-value)))))
 
 ;;TODO require from util
-(defn sleep-ms [] (js/Promise. (fn [r] (js/setTimeout r (rand-int-from-to 250 500)))))
+(defn sleep-ms
+  ([]
+   (sleep-ms (rand-int-from-to 250 500)))
+  ([time-in-ms]
+   (js/Promise. (fn [r] (js/setTimeout r time-in-ms)))))
 
 (defn escape-char
   "escape special characters"
@@ -23,7 +54,13 @@
    (interpose "\\=")
    (apply str)))
 
-(defn pull-schools-href-list [page {:keys [id href distrito concelho] :as concelho-map} results]
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; actual script logic
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn pull-schools-href-list
+  "Scrapes the actual href in each 'concelho' paginated page."
+  [page {:keys [id href distrito concelho] :as concelho-map} results]
   (p/let [schools (.evaluate page "a = []; Array.from(document.querySelectorAll('.IMTTLinkTituloA')).forEach(el => a.push(el.href));a")
           schools (doall (map #(hash-map :school-href %
                                          :concelho-href href
@@ -32,99 +69,66 @@
           results (into results schools)]
     results))
 
-(comment
-  (defp browser (.launch puppeteer #js {:headless false}))
-  (defp page (.newPage browser))
+(defn get-paginator-values
+  "Given a puppeteer page, returns a promise that contains the paginator values. Ex:
 
-  (p/let [s (pull-schools-href-list page {:id 123 :distrito "distrito" :concelho "concelho"} [])]
-    (prn "..." s)))
+  [1 2 3 >]
+  [< 1 2 3 >]
+  [< 1 2 3] "
+  [page]
+  (p/let [values (.$$eval page ".divPaginacaoSemMenu > input" (js/eval "(elements) => {return elements.map(el => el.value)}"))]
+    (mapv js->clj values)))
 
-;; 0 1 2 3
-;; 1 2 3 >
-;; < 1 2 3 >
-(defn cycle-paginators [page {:keys [id href distrito concelho] :as concelho-map} page-count next-page results]
-  (p/let [_ (.waitForSelector page ".divPaginacaoSemMenu > input")
-          paginator-handlers (.$$ page ".divPaginacaoSemMenu > input")
-          next-page-handler (nth paginator-handlers (if (= 2 next-page)
-                                                      1
-                                                      next-page)) ;;skip first one "<" after first page
-          _ (.click next-page-handler)
-          ;;_ (.goBack page)
-          ;;_ (.goForward page)
-          _ (sleep-ms)
-          _ (.waitForSelector page ".divPaginacaoSemMenu > input")
-          _ (prn "pulling for np:" next-page)
-          results (pull-schools-href-list page concelho-map results)
-          next-page (inc next-page)]
-    (if (<= next-page page-count)
-      (cycle-paginators page concelho-map page-count next-page results)
+(defn resolve-next-paginator-index
+  "given an array with the values representing the paginator (ex: [1 2 3 >])
+  and a human page number (ex: 1, 2 3),
+  returns the next paginator index or nil"
+  [paginator-values current-human-page]
+  (let [current-index (.indexOf paginator-values (str current-human-page))]
+    (if (< (inc current-index) (count paginator-values))
+      (inc current-index)
+      nil)))
+
+;; (resolve-next-paginator-index ["<" "1" "2" "3"] 3) => nil
+;; (resolve-next-paginator-index ["1" "2" "3" ">"] 1) => 1
+;; (resolve-next-paginator-index ["<" "1" "2" "3" ">"] 2) => 3
+
+(defn paginate-through-concelho
+  "given a page and a concelho map, paginate and fill the results array with each school link"
+  [page concelho-map current-human-page results]
+  (p/let [results (pull-schools-href-list page concelho-map results)
+          ;; no idea why I have to sleep and wait here...
+          _ (sleep-ms 1000)
+          ;; the paginator might never come if we have small concelhos...
+          #_#__ (.waitForSelector page ".divPaginacaoSemMenu > input")
+          paginator-elements (.$$ page ".divPaginacaoSemMenu > input")
+          paginator-values (get-paginator-values page)
+          next-paginator-index (resolve-next-paginator-index paginator-values current-human-page)
+          next-page (when next-paginator-index
+                      (nth paginator-elements next-paginator-index))]
+    (if next-page
+      (do (.click next-page)
+          (paginate-through-concelho page concelho-map (inc current-human-page) results))
       results)))
 
-(defn get-school-href [page concelho results]
-  (p/let [_ (.goto page (:href concelho))
-          _ (sleep-ms)
-          paginator-handles (.$$ page ".divPaginacaoSemMenu > input")
-          paginator-handles (drop-last paginator-handles);;drops the ">" next element
-          handlers-count (count paginator-handles)
-          no-pagination? (zero? handlers-count)
-          results (pull-schools-href-list page concelho results)]
-
-    (if no-pagination?
-      results
-      (cycle-paginators page concelho handlers-count 2 results))))
-
-(comment (p/let [browser (.launch puppeteer #js {:headless false})
-         page (.newPage browser)
-         _ (.goto page "https://www.imt-ip.pt/sites/IMTT/Portugues/EnsinoConducao/LocalizacaoEscolasConducao/Paginas/LocalizacaoEscolasConducao.aspx?Distrito=Porto&Concelho=Paredes")
-         paginator-handles (.$$ page ".divPaginacaoSemMenu > input")
-         paginator-handles (drop-last paginator-handles) ;;drops the ">" next element
-         handlers-count (count paginator-handles)]
-
-   (prn ">> " handlers-count) ))
-
-(comment
-  (def concelhos [{:distrito "Lisboa"
-                   :concelho "Lisboa"
-                   :href
-                   "https://www.imt-ip.pt/sites/IMTT/Portugues/EnsinoConducao/LocalizacaoEscolasConducao/Paginas/LocalizacaoEscolasConducao.aspx?Distrito=Faro&Concelho=Loul%C3%A9"}
-                  {:distrito "wtv"
-                   :concelho "wtv"
-                   :href "https://www.imt-ip.pt/sites/IMTT/Portugues/EnsinoConducao/LocalizacaoEscolasConducao/Paginas/LocalizacaoEscolasConducao.aspx?Distrito=Porto&Concelho=Paredes"}
-                  {:distrito "wtv"
-                   :concelho "wtv"
-                   :href
-                   "https://www.imt-ip.pt/sites/IMTT/Portugues/EnsinoConducao/LocalizacaoEscolasConducao/Paginas/LocalizacaoEscolasConducao.aspx?Distrito=Lisboa&Concelho=Lisboa"}])
-
-  (p/let [browser (.launch puppeteer #js {:headless false})
-          page (.newPage browser)
-          concelho-map (nth concelhos 2)
-          results (get-school-href page concelho-map [])]
-    (prn "pulled: "(count (set (map :school-href results))))))
-
-(defn pull-schools [page concelhos results]
+(defn pull-schools
+  "Get the schools for a list of concelhos"
+  [page concelhos results]
   (p/let [concelho (first concelhos)
           rest-concelho (rest concelhos)
           _ (println "working on schools at " (:concelho concelho) "/" (:distrito concelho) "\n")
           _ (println (:href concelho) "\n")
-          results (get-school-href page concelho results)
+          page-number 1 ;; actuall page in the paginator 1, 2 3, etc. Don't confuse it with an index it's just the human page number
+          - (.goto page (:href concelho))
+          results (paginate-through-concelho page concelho page-number results)
           _ (println "schools so far " (count results) "\n")]
     (if (empty? rest-concelho)
       results
       (pull-schools page rest-concelho results))))
 
-(comment
-  (p/let [browser (.launch puppeteer #js {:headless false})
-          page (.newPage browser)
-          r (pull-schools page
-                          [{:href "https://www.imt-ip.pt/sites/IMTT/Portugues/EnsinoConducao/LocalizacaoEscolasConducao/Paginas/LocalizacaoEscolasConducao.aspx?Distrito=Faro&Concelho=Loul%C3%A9",
-                            :concelho "wtv"
-                            :distrito "wtv"}]
-                          [])]
-    (prn (set (map :school-href r)))))
-
-(def anchors-query "var a = []; Array.from(document.querySelectorAll('a')).forEach(el => {a.push({href: el.href, id: el.id, html: el.innerHTML })});a")
-
-(defn pull-concelhos [page districts results]
+(defn pull-concelhos
+  "Recursively pulls all concelhos for a list of districts. Returns all concelhos in 'districts'."
+  [page districts results]
   (p/let [{:keys [id href html]} (first districts)
           districts (rest districts)
           _ (.goto page href)
@@ -147,25 +151,13 @@
       results
       (pull-concelhos page districts results))))
 
-(comment
- (defp browser (.launch puppeteer #js {:headless false}))
- (defp page (.newPage browser))
- (p/let [browser (.launch puppeteer #js {:headless false})
-         page (.newPage browser)]
-   (pull-concelhos page
-                   [{:href "https://www.imt-ip.pt/sites/IMTT/Portugues/EnsinoConducao/LocalizacaoEscolasConducao/Paginas/LocalizacaoEscolasConducao.aspx?Distrito=Faro&Concelho=Loul%C3%A9",
-                     :id "Distrito\\=Loule",
-                     :html "Loule"}])))
-
-(def base-url "https://www.imt-ip.pt")
-
-(def district-url "/sites/IMTT/Portugues/EnsinoConducao/LocalizacaoEscolasConducao/Paginas/LocalizacaoEscolasConducao.aspx")
-
-(defn pull-districts [page]
+(defn pull-districts
+  "Given a puppeteer page for the IMT schools, extracts a list of districts."
+  [page]
  (p/let [_ (.goto page (str base-url district-url))
          _ (sleep-ms)
          anchors (.evaluate page anchors-query)
-         districts (->> anchors
+         districts (->> anchors ;;gets all links each page
                         (map js->clj)
                         (map (fn [el]
                                (->> el vec (map (fn [[f l]]
@@ -178,14 +170,11 @@
                         doall)]
    districts))
 
-(comment
-  (p/let [browser (.launch puppeteer #js {:headless false})
-          page (.newPage browser)
-          districts (pull-districts page)]
-    (prn "Districts: " (count districts))))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Main
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-
-(p/let [browser (.launch puppeteer #js {:headless true})
+(p/let [browser (.launch puppeteer #js {:headless false #_ true})
         page (.newPage browser)
         districts (pull-districts page)
         #_#_districts (subvec (vec districts)  0 1)
@@ -197,9 +186,75 @@
   (.writeFileSync fs "./temp/hrefs.json" school-string))
 
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Examples for debug and other
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; npx nbb nrepl-server/
+
+
 (comment
-  (defmacro defp
-    "Define var when promise is resolved"
-    [binding expr]
-    `(-> ~expr (.then (fn [val]
-                        (def ~binding val))))))
+
+  ;; Util functions
+
+
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  ;; 1. Get a list of schools with pull-schools-href-list
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+  (do
+    ;; Open a browser and navigate to page first
+    (defp browser (.launch puppeteer #js {:headless true}))
+    (defp page (.newPage browser))
+    (def imt-page-with-schools
+      "https://www.imt-ip.pt/sites/IMTT/Portugues/EnsinoConducao/LocalizacaoEscolasConducao/Paginas/LocalizacaoEscolasConducao.aspx?Distrito=Faro&Concelho=Loul%C3%A9")
+
+    (.goto page imt-page-with-schools)
+
+    (defp hrefs-list-example (pull-schools-href-list page {:id 123 :distrito "distrito" :concelho "concelho"} [])))
+
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  ;; 2. Paginator logic cycle-paginators and get-school-href
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+  (defp browser (.launch puppeteer #js {:headless false}))
+  (defp page (.newPage browser))
+  (def imt-page-with-schools
+    ;; small concelho with 1 page
+    #_"https://www.imt-ip.pt/sites/IMTT/Portugues/EnsinoConducao/LocalizacaoEscolasConducao/Paginas/LocalizacaoEscolasConducao.aspx?Distrito=Viseu&Concelho=Cinf%C3%A3es"
+    ;; lisbon
+    "https://www.imt-ip.pt/sites/IMTT/Portugues/EnsinoConducao/LocalizacaoEscolasConducao/Paginas/LocalizacaoEscolasConducao.aspx?Distrito=Lisboa&Concelho=Lisboa")
+  (.goto page imt-page-with-schools)
+
+  (def concelho-map {:id 123 :distrito "distrito" :concelho "concelho" :href imt-page-with-schools})
+
+  (defp test (paginate-through-concelho page concelho-map 1 []))
+
+  (identity test)
+
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  ;; 3. pull-concelhos
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+  (defp browser (.launch puppeteer #js {:headless false}))
+  (defp page (.newPage browser))
+  (p/let [browser (.launch puppeteer #js {:headless false})
+          page (.newPage browser)]
+    (pull-concelhos page
+                    [{:href "https://www.imt-ip.pt/sites/IMTT/Portugues/EnsinoConducao/LocalizacaoEscolasConducao/Paginas/LocalizacaoEscolasConducao.aspx?Distrito=Faro&Concelho=Loul%C3%A9",
+                      :id "Distrito\\=Loule",
+                      :html "Loule"}]))
+
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  ;; 4 pull-chools
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+  (p/let [browser (.launch puppeteer #js {:headless false})
+          page (.newPage browser)
+          r (pull-schools page
+                          [{:href "https://www.imt-ip.pt/sites/IMTT/Portugues/EnsinoConducao/LocalizacaoEscolasConducao/Paginas/LocalizacaoEscolasConducao.aspx?Distrito=Faro&Concelho=Loul%C3%A9",
+                            :concelho "wtv"
+                            :distrito "wtv"}]
+                          [])]
+    (prn (set (map :school-href r))))
+  )
